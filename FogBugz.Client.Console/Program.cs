60778fbd41718bbdb3515c8113e750628e020969
Neal Chapman <nchapman@pcatg.com>
2023-02-09 13:08:26 -0500

    Started working on command line arg parsing.

--- FogBugz.Client.Console/Program.cs
+++ FogBugz.Client.Console/Program.cs
@@ -1,5 +1,16 @@
 ﻿using Microsoft.Extensions.Configuration;
 
+var builder = new ConfigurationBuilder();
+builder.AddCommandLine(args);
+
+var config = builder.Build();
+
+Console.WriteLine($"Key1: '{config["Key1"]}'");
+Console.WriteLine($"Key2: '{config["Key2"]}'");
+Console.WriteLine($"Key3: '{config["Key3"]}'");
+Console.WriteLine($"Key4: '{config["Key4"]}'");
+Console.WriteLine($"Key5: '{config["Key5"]}'");
+
 Console.WriteLine("Welcome to FogBugz Client!");
 Console.WriteLine("");
 
